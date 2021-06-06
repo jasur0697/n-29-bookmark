ff9cd6e30e485eddcf3f827eb06ca20bd40d876f
JasurbekNurmamatov <63550009+JasurbekNurmamatov@users.noreply.github.com>
2021-06-06 18:58:15 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
-var elOpenModalButton = document.querySelector('.js-open-modal-button');
-var elCloseModalButton = document.querySelector('.js-close-modal-button');
-var elModal = document.querySelector('.js-modal');
 
-elOpenModalButton.addEventListener('click', function () {
-  elModal.classList.add('modal-open');
+var elsFeatItem = document.querySelectorAll('.features__item');
+var elsFeatBtn = document.querySelectorAll('.features__button');
+
+elsFeatBtn.forEach(function (link) {
+  link.addEventListener('click', function () {
+    elsFeatItem.forEach(function (item) {
+      item.classList.remove('features__button-active');
+    });
+
+    link.parentElement.classList.add('features__button-active');
+  });
 });
 
-elCloseModalButton.addEventListener('click', function () {
-  elModal.classList.remove('modal-open');
-});
 
 
-// fon o'zgartirish
-var elChangeTextBgButton = document.querySelector('.js-change-text-bg');
-var elText = document.querySelector('.text');
 
-elChangeTextBgButton.addEventListener('click', function () {
-  elText.classList.toggle('text-bg');
+var elsQuestionsItem = document.querySelectorAll('.questions__item');
+var elsQuestionsButton = document.querySelectorAll('.questions__item--button');
+elsQuestionsButton.forEach(function (button) {
+  button.addEventListener('click', function () {
+    elsQuestionsItem.forEach(function (item) {
+      item.classList.remove('questions__item--active');
+    });
+    button.closest('.questions__item').classList.add('questions__item--active');
+  });
 });
